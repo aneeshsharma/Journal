31779cc7b1257cf2c1f789b3b2b04df8179c2dce
Anish Sharma <aneeshsharma15@outlook.com>
2019-11-06 15:21:22 +0530

    Created a basic window

---
+++ main.js
@@ -0,0 +1,38 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+const electron = require('electron')
+
+// Enable live reload for Electron too
+require('electron-reload')(__dirname, {
+    // Note that the path to electron may vary according to the main file
+    electron: require(`${__dirname}/node_modules/electron`)
+});
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    frame: false
+  })
+
+
+  mainWindow.loadFile('index.html')
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
